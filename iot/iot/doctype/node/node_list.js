f0eaf915218d6b383a6a9b5184c9048a0e40acb7
perkasa <perkasajob@gmail.com>
2018-06-03 16:22:54 +0700

    adding node_list.js

---
+++ iot/iot/doctype/node/node_list.js
@@ -0,0 +1,7 @@
+frappe.listview_settings['Node'] = {
+	onload: function (listview) {
+		listview.page.add_menu_item(__("Dashboard"), function () {
+			window.location = window.location.origin + '/iot'
+		});
+	}
+};
